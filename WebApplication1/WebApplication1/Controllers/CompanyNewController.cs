383b93116d6467562795f6353a47ae005aa78e74
Tomasz Hajduga <hajduga1507@gmail.com>
2020-11-14 13:43:33 +0100

    Second lesson

---
+++ WebApplication1/WebApplication1/Controllers/CompanyNewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyNewController : ControllerBase
+    {
+        [HttpPost]
+        [Route("AddNewItem")]
+        public IActionResult AddNewItem(CompanyModel companyModel)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/CompanyAddedViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CompanyAddedViewModel
+    {
+        public int NumberOfCharsInName { get; set; }
+        public int NumberOfCharsInDescription { get; set; }
+        public bool IsHidden { get; set; }
+
+    }
+}
